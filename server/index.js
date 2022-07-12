e5c3a22bbd3b2b1137355860995005a235e75bbf
maorte <maor884@gmail.com>
2022-07-12 18:51:30 +0300

    implemented heroku deploy

--- server/index.js
+++ server/index.js
@@ -62,7 +62,7 @@ app.listen(port, () => {
 });
 
 //deploy to heroku
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('/*', (req, res) => {
-//    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-// });
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
